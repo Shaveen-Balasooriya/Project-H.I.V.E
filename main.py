5b0771444da494a11b411f81e0a54c721de878e6
Shaveen Balasooriya <shaveenb2004@outlook.com>
2025-03-27 14:58:28 +0530

    [refactor]: New Structure
    Removed all the non-essential files for now.
    
    Signed-off-by: Shaveen Balasooriya <shaveenb2004@outlook.com>

--- main.py
+++ main.py
@@ -1,26 +1,8 @@
-import podman
-
-def build_image(client) -> None:
-    try:
-        print('Building SSH Honeypot...')
-        client.images.build(path='./Honeypots/SSH', tag='SSH-Honeypot', rm=True)
-        print('Done!')
-    except Exception as e:
-        print(f'Error building image: {str(e)}')
+from podman_control.Podman import Podman
 
 def main() -> None:
-    try:
-        # Connect to the Podman API
-        client = podman.PodmanClient(base_url='tcp://localhost:8888')  # Adjust if needed
-        # Check if Podman is running
-        if client.ping():
-            print('Connected to Podman and service is running')
-            build_image(client=client)
-        else:
-            print('Connected to Podman but service is not running')
-    except Exception as e:
-        print(f'Error: {str(e)}')
+    client = Podman()
+    client.check_config()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
