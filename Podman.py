24e4f04a8d8f0144ba6807b6c34fa0d698c585b2
Shaveen Balasooriya <shaveenb2004@outlook.com>
2025-03-30 15:53:28 +0530

    [feat]: SSH Honeypot building
    Was able to fix the code to properly load the Dockerfile inside the type of the honeypot and then build the image if it doesn't already exist.
    
    Signed-off-by: Shaveen-Balasooriya <shaveenb2004@outlook.com>

--- Podman.py
+++ Podman.py
@@ -3,7 +3,6 @@ import os
 
 class Podman:
     url = 'unix:///tmp/podman.sock'
-    image = 'H.I.V.E-Image'
 
     def __init__(self) -> None:
         self.client = podman.PodmanClient(base_url=Podman.url)
@@ -25,23 +24,25 @@ class Podman:
     #     except Exception as e:
     #         print(f"Error building image '{image}': {e}")
 
-    def build_image(self, honeypot_type: str) -> bool:
-        image = f'H.I.V.E-{honeypot_type.upper()}-Image'
+    def build_image(self, honeypot_type: str) -> None:
+        image = f'h.i.v.e-image-{honeypot_type.lower()}'
+        dockerfile_path = os.path.join('honeypots', honeypot_type)
 
-        # Determine the build context directory.
-        context_path = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            'honeypots',
-            honeypot_type.lower()
-        )
-
-        # Use the file named 'Dockerfile' in that directory.
-        self.client.images.build(
-            path=context_path,  # Set the build context directory
-            dockerfile='Dockerfile',  # Specify the Dockerfile name
-            tag=image,
-            rm=True,
-        )
-
-        print(f"Image '{image}' built successfully.")
-        return True
+        # Check if the image already exists
+        if self.client.images.exists(image):
+            print(f"Image '{image}' already exists.")
+        else:
+            try:
+                print('Building image...')
+                # Build from the current directory with the Dockerfile path specified
+                self.client.images.build(
+                    path=dockerfile_path, # Path to the Dockerfile directory
+                    dockerfile='Dockerfile', # Name of the Dockerfile
+                    tag=image,
+                    rm=True,
+                )
+                print(f"Image '{image}' built successfully.")
+            except PermissionError:
+                print("Permission denied. Please run with sudo.")
+            except Exception as e:
+                print(f"Error building image: {e}")
