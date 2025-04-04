311a1757de91b10e6df966f78025760598b90027
Shaveen-Balasooriya <shaveenb2004@outlook.com>
2025-04-04 16:29:34 +0530

    [feat]: Honeypot creation and deployment
    I added the ability to properly create a honeypot and then deploy it.
    
    Signed-off-by: Shaveen-Balasooriya <shaveenb2004@outlook.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Podman import Podman
 
 def main() -> None:
     client = Podman()
-    client.build_image('ssh')
-
+    honeypot = client.create_honeypot(honeypot_type='ssh'.lower(), port_to_bind=2222)
+    client.start_honeypot()
 if __name__ == '__main__':
     main()
