0afa41e47cc5a902c323137b4f06a8fc59eed876
Shaveen Balasooriya <shaveenb2004@outlook.com>
2025-04-06 11:57:24 +0530

    [feat]: Honeypot Manager
    Newly updated Honeypot Manager.
    
    Signed-off-by: Shaveen Balasooriya <shaveenb2004@outlook.com>

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from Podman import Podman
+import time
+from honeypot_manager.honeypot_manager import HoneypotManager
+from honeypot_manager.Honeypot import Honeypot
+
 
 def main() -> None:
-    client = Podman()
-    honeypot = client.create_honeypot(honeypot_type='ssh'.lower(), port_to_bind=2222)
-    client.start_honeypot()
+    honeypot = Honeypot()
+    print('Creating Honeypot...')
+    honeypot.create_honeypot('ssh', 2222)
+    print('Starting Honeypot...')
+    honeypot.start_honeypot()
+    print('Fetching all Honeypots...')
+    honeypots = HoneypotManager()
+    h = honeypots.fetch_all_honeypots()
+    for honey in h:
+        print(f'Honeypot Name: {honey.honeypot_name}')
+        print(f'Honeypot Status: {honey.honeypot_status}')
+        print(f'Honeypot Type: {honey.honeypot_type}')
+        print(f'Honeypot Port: {honey.honeypot_port}')
+        print('-------------------')
 if __name__ == '__main__':
     main()
